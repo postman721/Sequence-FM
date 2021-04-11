a699edc4eb703fdba9bd68da9b10ad9c57faea83
JJ Posti <jjposti@techtimejourney.net>
2021-04-11 17:10:51 +0300

    Adding theme changing file.

---
+++ theme.py
@@ -0,0 +1,14 @@
+
+#Set your theme here by changing the theme file between " "
+
+#Supported values: 
+
+		#	"default"	  # Default theme with blue colors.
+		# 	"midnight"    # Really dark theme.
+		# 	"old"         # Old default theme.
+		# 	"yellow"      # Colorful theme.
+		#   ""		      # Basic QT without CSS theming.
+
+theme="default"
+
+
